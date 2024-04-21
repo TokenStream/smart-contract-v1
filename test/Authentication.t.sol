81aa70d0f7844bceccb6835a7db42681ab3efda6
JoshdfG <agboolajoshua161@gmail.com>
2024-04-21 23:10:56 +0100

    tests

--- test/Authentication.t.sol
+++ test/Authentication.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Test, console} from "forge-std/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import "../src/Authentication.sol";
 
 contract AuthenticationTest is Test {
