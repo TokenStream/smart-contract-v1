697aa419cb8c8a156d4ac0f2bc28294fce7a7b59
JoshdfG <agboolajoshua161@gmail.com>
2024-04-21 22:54:35 +0100

    tests

--- test/Authentication.t.sol
+++ test/Authentication.t.sol
@@ -45,4 +45,22 @@ contract AuthenticationTest is Test {
         assertTrue(auth.usernameExist(name1));
         assertFalse(auth.usernameExist(name2));
     }
+
+    function testGetAllUsers() public {
+        bytes memory name1 = bytes("frank");
+        bytes memory name2 = bytes("grace");
+
+        vm.prank(address(this));
+        auth.createAccount(name1);
+
+        vm.prank(address(0x1234));
+        auth.createAccount(name2);
+
+        Authentication.User[] memory users = auth.getAllUsers();
+        assertEq(users.length, 2);
+        assertEq(users[0].name, name1);
+        assertEq(users[0].address_, address(this));
+        assertEq(users[1].name, name2);
+        assertEq(users[1].address_, address(0x1234));
+    }
 }
