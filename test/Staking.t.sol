0b4af878deb9810598365a9f0a3279d8c46d5800
JoshdfG <agboolajoshua161@gmail.com>
2024-04-26 03:53:19 +0100

    testStaking & testUnstaking

--- test/Staking.t.sol
+++ test/Staking.t.sol
@@ -11,11 +11,12 @@ contract StakingPoolTest is Test {
     RewardToken rewardToken;
     StakingToken stakingToken;
     address owner = address(0xc);
+    address staker = address(0xd);
 
     function setUp() public {
         owner = address(this);
         rewardToken = new RewardToken(address(this));
-        stakingToken = new StakingToken(address(rewardToken));
+        stakingToken = new StakingToken(address(owner));
         stakingPool = new StakingPool(
             address(stakingToken),
             address(rewardToken)
@@ -43,5 +44,48 @@ contract StakingPoolTest is Test {
             initialRewardReserve,
             "Reward reserve should be 100 tokens"
         );
+    }
+
+    function testStake() public {
+        uint256 initialRewardRate = 100;
+        uint256 initialRewardReserve = 100E18;
+        rewardToken.mint(address(this), initialRewardReserve);
+        rewardToken.approve(address(stakingPool), initialRewardReserve);
+
+        stakingPool.createPool(initialRewardRate);
+
+        uint256 stakeAmount = 5e18;
+        vm.startPrank(address(owner));
+        stakingToken.mint(staker, stakeAmount);
+        vm.stopPrank();
+
+        vm.startPrank(address(staker));
+        stakingToken.approve(address(stakingPool), stakeAmount);
+        stakingPool.stake(0, stakeAmount);
+
+        uint256 stakeBal = stakingPool.getUserStakeBalance(0, address(staker));
+        assertEq(stakeBal, stakeAmount, "Staking error");
+    }
+
+    function testUnstake() public {
+        uint256 initialRewardRate = 100;
+        uint256 initialRewardReserve = 100E18;
+        rewardToken.mint(address(this), initialRewardReserve);
+        rewardToken.approve(address(stakingPool), initialRewardReserve);
+
+        stakingPool.createPool(initialRewardRate);
+
+        uint256 stakeAmount = 5e18;
+        vm.startPrank(address(owner));
+        stakingToken.mint(staker, stakeAmount);
+        vm.stopPrank();
+
+        vm.startPrank(address(staker));
+        stakingToken.approve(address(stakingPool), stakeAmount);
+        stakingPool.stake(0, stakeAmount);
+        stakingPool.unstake(0);
+
+        uint256 stakeBal = stakingPool.getUserStakeBalance(0, address(staker));
+        assertEq(stakeBal, 0, "Unstaking error");
     }
 }
