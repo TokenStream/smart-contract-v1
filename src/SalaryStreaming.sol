5f7597102af21783f81aa5ae4ffafe382a846ea9
JoshdfG <agboolajoshua161@gmail.com>
2024-04-29 02:10:06 +0100

    Updated salary streaming contract

--- src/SalaryStreaming.sol
+++ src/SalaryStreaming.sol
@@ -1,54 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+error ADDRESS_ZERO_NOT_ALLOWED();
+error YOU_ARE_NOT_THE_ADMIN();
+error STERAM_IS_NOT_ACTIVE();
+error STREAM_IS_ALREADY_ACTIVE();
+error STREAM_NOT_FOUND_AT_THE_GIVEN_ADDRESS();
+
 contract SalaryStreaming {
+    enum IntervalType {
+        None,
+        Daily,
+        Monthly
+    }
+
     struct Stream {
         uint256 id;
         address recipient;
         uint256 amount;
         uint256 lastPayment;
         uint256 startTime;
-        uint256 interval;
+        IntervalType intervalType;
         bool active;
     }
 
-    // Array to store all Stream structs
     Stream[] public streams;
-
-    // Mapping to track stream IDs by recipient address
     mapping(address => uint256) public streamIdsByAddress;
     address public admin;
-
-    // Counter for generating unique stream IDs
     uint256 private idCounter = 0;
 
     event StreamPaused(uint256 indexed streamId, address indexed recipient);
     event StreamResumed(uint256 indexed streamId, address indexed recipient);
-    event StreamCreated(uint256 indexed streamId, address indexed recipient);
+    event StreamCreated(
+        uint256 indexed streamId,
+        address indexed recipient,
+        IntervalType intervalType
+    );
     event StreamStopped(uint256 indexed streamId, address indexed recipient);
 
     constructor() {
         admin = msg.sender;
     }
 
-    modifier onlyAdmin() {
-        require(msg.sender == admin, "Caller is not the admin");
-        _;
-    }
-
-    // Define a struct to represent the stream details
     struct StreamDetails {
         address recipient;
         uint256 amount;
-        uint256 interval;
     }
 
-    // Modify the createStream function to accept an array of StreamDetails structs
-    function createStream(StreamDetails[] calldata _streamDetails) external {
-        require(msg.sender != address(0), "ADDRESS_ZERO_NOT_ALLOWED");
+    function createStream(
+        StreamDetails[] calldata _streamDetails,
+        uint8[] calldata _intervalTypes
+    ) external {
+        require(msg.sender == admin, "Caller is not the admin");
+        require(
+            _streamDetails.length == _intervalTypes.length,
+            "Stream details and interval types arrays must have the same length"
+        );
 
         for (uint256 i = 0; i < _streamDetails.length; i++) {
-            // Increment the ID counter for each stream
             uint256 _id = idCounter++;
             streams.push(
                 Stream({
@@ -57,48 +66,81 @@ contract SalaryStreaming {
                     amount: _streamDetails[i].amount,
                     lastPayment: block.timestamp,
                     startTime: block.timestamp,
-                    interval: _streamDetails[i].interval,
+                    intervalType: IntervalType(_intervalTypes[i]),
                     active: true
                 })
             );
 
-            // Update the mapping with the new stream ID
             streamIdsByAddress[_streamDetails[i].recipient] = _id;
-
-            emit StreamCreated(_id, _streamDetails[i].recipient);
+            emit StreamCreated(
+                _id,
+                _streamDetails[i].recipient,
+                IntervalType(_intervalTypes[i])
+            );
         }
     }
 
-    function pauseStreamByAddress(address _recipient) external onlyAdmin {
+    function onlyAdmin() private view {
+        if (msg.sender != admin) {
+            revert YOU_ARE_NOT_THE_ADMIN();
+        }
+    }
+
+    function pauseStreamByAddress(address _recipient) external {
+        onlyAdmin();
         uint256 streamId = streamIdsByAddress[_recipient];
-        require(streamId != 0, "Stream not found for the given address");
-        require(streams[streamId].active, "Stream not active");
+        if (streamId == 0) {
+            revert STREAM_NOT_FOUND_AT_THE_GIVEN_ADDRESS();
+        }
+
+        if (!streams[streamId].active) {
+            revert STERAM_IS_NOT_ACTIVE();
+        }
+
         streams[streamId].active = false;
         emit StreamPaused(streamId, _recipient);
     }
 
     // Function to resume a stream by recipient address
-    function resumeStreamByAddress(address _recipient) external onlyAdmin {
+    function resumeStreamByAddress(address _recipient) external {
+        onlyAdmin();
         uint256 streamId = streamIdsByAddress[_recipient];
-        require(streamId != 0, "Stream not found for the given address");
-        require(!streams[streamId].active, "Stream already active");
+
+        if (streamId == 0) {
+            revert STREAM_NOT_FOUND_AT_THE_GIVEN_ADDRESS();
+        }
+
+        if (!streams[streamId].active) {
+            revert STERAM_IS_NOT_ACTIVE();
+        }
+
         streams[streamId].active = true;
         emit StreamResumed(streamId, _recipient);
     }
 
-    function stopStreamByAddress(address _recipient) external onlyAdmin {
+    function stopStreamByAddress(address _recipient) external {
+        onlyAdmin();
         uint256 streamId = streamIdsByAddress[_recipient];
-        require(streamId != 0, "Stream not found for the given address");
-        require(streams[streamId].active, "Stream not active");
+
+        if (streamId == 0) {
+            revert STREAM_NOT_FOUND_AT_THE_GIVEN_ADDRESS();
+        }
+
+        if (!streams[streamId].active) {
+            revert STERAM_IS_NOT_ACTIVE();
+        }
+
         streams[streamId].active = false;
         emit StreamStopped(streamId, _recipient);
     }
 }
 
-// [["0xb9a00a47c522f6ec6b257c545542361c00f02fcd",100,10],["0xb9a00a47c522f6ec6b257c545542361c00f02fcd",200,10]]
+// [["0xb9a00a47c522f6ec6b257c545542361c00f02fcd",100,1],["0x742d35Cc6634C0532925a3b844Bc454e4438f44e",200,2]]
+// [["0xb9a00a47c522f6ec6b257c545542361c00f02fcd",100],["0x742d35Cc6634C0532925a3b844Bc454e4438f44e",200]]
 
 // 0x742d35Cc6634C0532925a3b844Bc454e4438f44e
 // 0x5aeda56215b167893e80b4fe645ba6d5bab767de
 // 0x27d8d15cbc94527cadf5ec14b69519ae23288b95
 // 0x78731d3Ca6b7e34aC0F824c42a7cC18A495cabaB
 // 0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C
+// [["0xb9a00a47c522f6ec6b257c545542361c00f02fcd",100,2],["0x5aeda56215b167893e80b4fe645ba6d5bab767de",200,1]]
