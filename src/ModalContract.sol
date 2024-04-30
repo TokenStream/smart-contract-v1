ccd16149a253b22403d44ee83c35e78b11b7fd49
JoshdfG <agboolajoshua161@gmail.com>
2024-04-30 17:08:04 +0100

    modal_update

--- src/ModalContract.sol
+++ src/ModalContract.sol
@@ -1,143 +1,75 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./SubscriptionService.sol";
 import "./ERC20Token.sol";
-import "./Interfaces/IAuthentication.sol";
 import "./Interfaces/IRewardToken.sol";
-import "./Interfaces/ISalaryStreaming.sol";
-import "./Interfaces/IStakingPool.sol";
-import "./Interfaces/ISubscriptionService.sol";
+
+error ONLY_THE_OWNER_CAN_PERFORM_THIS_ACTION();
+error INSUFFICIENT_BALANCE();
+error YOU_DO_NOT_HAVE_REWARDS();
 
 contract ModalContract {
-    SubscriptionService public subscriptionService;
-    IAuthentication public authentication;
-    IRewardToken public rewardToken;
-    IStakingPool public stakingPool;
-    ISalaryStreaming public salaryStreaming;
-    IERC20 public token;
+    IERC20 public OPToken;
+    IRewardToken public TriverToken;
     address public owner;
     address nextOwner;
     uint256 public constant REWARD_RATE = 5;
 
-    // struct SubscriptionPlan {
-    //     string name;
-    //     uint256 fee;
-    //     uint256 interval;
-    //     address paymentAddress;
-    //     bool active;
-    // }
-
     event DepositSuccessiful(address indexed user, uint256 _amount);
     event WithdrawalSuccessiful(address indexed user, uint256 _amount);
 
-    constructor(
-        address _authentication,
-        address _rewardToken,
-        address _stakingPool,
-        address _subscriptionService,
-        address _salaryStreaming,
-        address _token
-    ) {
-        authentication = IAuthentication(_authentication);
-        rewardToken = IRewardToken(_rewardToken);
-        stakingPool = IStakingPool(_stakingPool);
-        subscriptionService = SubscriptionService(_subscriptionService);
-        salaryStreaming = ISalaryStreaming(_salaryStreaming);
-        token = IERC20(_token); //Initialize token and owner
+    constructor(address _TriverToken, address _OPToken) {
+        OPToken = IERC20(_OPToken);
+        //Initialize token and owner
+        TriverToken = IRewardToken(_TriverToken);
         owner = msg.sender;
     }
 
-    mapping(address => uint256) public balances; // user balance in the contract
+    // user balance in the contract
+    mapping(address => uint256) public balances;
 
-    //Check for the owner
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can perform this action");
-        _;
+    function onlyOwner() private view {
+        if (msg.sender != owner) {
+            revert ONLY_THE_OWNER_CAN_PERFORM_THIS_ACTION();
+        }
     }
 
     function deposit(uint256 _amount) external {
-        require(token.balanceOf(msg.sender) >= _amount, "Insufficient balance");
-        token.transfer(address(this), _amount);
+        require(
+            OPToken.balanceOf(msg.sender) >= _amount,
+            "Insufficient balance"
+        );
+        OPToken.transfer(address(this), _amount);
         balances[msg.sender] += _amount;
         emit DepositSuccessiful(msg.sender, _amount);
     }
 
     // Function to withdraw from the contract
     function withdraw(uint256 _amount) external {
-        require(balances[msg.sender] > _amount, "Insufficient balance");
+        if (balances[msg.sender] < _amount) {
+            revert INSUFFICIENT_BALANCE();
+        }
         balances[msg.sender] - _amount;
-        token.transfer(msg.sender, _amount);
+        OPToken.transfer(msg.sender, _amount);
         emit WithdrawalSuccessiful(msg.sender, _amount);
     }
 
     function platformRewards(address _user) external {
         uint256 userBalance = balances[_user];
 
-        require(userBalance > 0, "No balance to reward");
+        if (userBalance <= 0) {
+            revert YOU_DO_NOT_HAVE_REWARDS();
+        }
 
         uint256 rewardAmount = (userBalance * REWARD_RATE) / 10000;
 
         // Transfer the rewards to the user
-        rewardToken.transfer(_user, rewardAmount);
-    }
-
-    function addSubscriptionPlan(string memory _name, uint256 _fee, uint256 _interval, address _paymentAddress)
-        external
-    {
-        subscriptionService.addSubscriptionPlan(_name, _fee, _interval, _paymentAddress);
-    }
-
-    // Function to create an account
-    function createAccount(bytes calldata _name) external {
-        authentication.createAccount(_name);
-    }
-
-    // Functions to interact with the subscription service
-    function startSubscription(uint256 planId) external {
-        subscriptionService.startSubscription(planId);
-    }
-
-    function pauseSubscription(uint256 planId) external {
-        subscriptionService.startSubscription(planId);
-    }
-
-    function resumeSubscription(uint256 planId) external {
-        subscriptionService.resumeSubscription(planId);
-    }
-
-    function stopSubscription(uint256 planId) external {
-        subscriptionService.stopSubscription(planId);
-    }
-
-    function deactivateSubscriptionPlan(uint256 planId) external {
-        subscriptionService.startSubscription(planId);
-    }
-
-    function createCustomSubscription(string memory _name, uint256 _fee, uint256 _interval, address _paymentAddress)
-        external
-    {
-        subscriptionService.createCustomSubscription(_name, _fee, _interval, _paymentAddress);
-    }
-    // Functions to interact with the staking pool
-
-    function stake(uint256 _poolID, uint256 _amount) external {
-        stakingPool.stake(_poolID, _amount);
-    }
-
-    function createPool(uint256 _rewardRate) external {
-        stakingPool.createPool(_rewardRate);
-    }
-
-    // Functions to interact with the salary streaming service
-    function createStream(address[] calldata recipients, uint256[] calldata amount, uint8[] calldata intervalTypes)
-        external
-    {
-        salaryStreaming.createStream(recipients, amount, intervalTypes);
+        TriverToken.transfer(_user, rewardAmount);
     }
 
     //change ownership
-    function transferOwnership(address _newOwner) external onlyOwner {
+    function transferOwnership(address _newOwner) external {
+        onlyOwner();
         nextOwner = _newOwner;
     }
 
